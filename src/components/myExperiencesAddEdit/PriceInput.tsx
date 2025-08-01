ae13975ed469035d6bcfa7e89fa74b7a7468f888
YuWonKyu <duck01777@naver.com>
2025-08-02 01:29:04 +0900

    ♻️refactor: PriceInput 컴포넌트의 입력 처리 로직 개선

--- src/components/myExperiencesAddEdit/PriceInput.tsx
+++ src/components/myExperiencesAddEdit/PriceInput.tsx
@@ -9,22 +9,40 @@ const PriceInput = <T extends FieldValues>({
   register,
   path,
 }: PriceInputProps<T>) => {
+  // IME, 붙여넣기 등 모든 입력에서 숫자만 남기기
+  const handleInput = (
+    e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>,
+  ) => {
+    // input 엘리먼트에서만 동작
+    if (e.currentTarget instanceof HTMLInputElement) {
+      const onlyNums = e.currentTarget.value.replace(/[^0-9]/g, '');
+      if (e.currentTarget.value !== onlyNums) {
+        e.currentTarget.value = onlyNums;
+        // react-hook-form 값도 동기화
+        const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+          window.HTMLInputElement.prototype,
+          'value',
+        )?.set;
+        nativeInputValueSetter?.call(e.currentTarget, onlyNums);
+        e.currentTarget.dispatchEvent(new Event('input', { bubbles: true }));
+      }
+    }
+  };
+
   return (
-    <div className="mb-24">
+    <div className='mb-24'>
       <Input
-        label="가격"
-        labelClassName="text-16-b"
-        placeholder="체험 금액을 입력해 주세요"
+        label='가격'
+        labelClassName='text-16-b'
+        placeholder='체험 금액을 입력해 주세요'
         error={error}
         value={value}
-        inputMode="numeric"
+        inputMode='numeric'
+        type='text'
+        onInput={handleInput}
         {...register(path as Path<T>)}
       />
-      {value && (
-        <div className="text-14-r mt-2 text-gray-500">
-          {formatPrice(Number(value))}
-        </div>
-      )}
+      {value && <div className='text-14-r mt-2 text-gray-500'>{formatPrice(Number(value))}</div>}
     </div>
   );
 };
