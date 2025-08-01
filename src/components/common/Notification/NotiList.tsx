ee983b1ef234a07c23965c10540afb27a0e52186
geesunny <jiseonyang08@gmail.com>
2025-08-01 12:33:02 +0900

    ♻️refactor: 알림 드롭다운 부분 px to rem

--- src/components/common/Notification/NotiList.tsx
+++ src/components/common/Notification/NotiList.tsx
@@ -12,6 +12,7 @@ interface NotificationType {
   content: string;
   createdAt: string;
 }
+
 interface NotiListProps {
   setHasNewNotification?: (val: boolean) => void;
 }
@@ -62,7 +63,7 @@ const NotiList = ({ setHasNewNotification }: NotiListProps) => {
   return (
     <div
       className={
-        'max-h-[360px] w-[92vw] max-w-[320px] overflow-y-auto rounded-[12px] bg-white shadow-lg transition-all'
+        'max-h-[22.5rem] w-[92vw] max-w-[20rem] overflow-y-auto rounded-[0.75rem] bg-white shadow-lg transition-all'
       }
     >
       <h3 className='text-16-b border-b border-gray-100 px-16 pt-16 pb-8 text-gray-950 sm:px-16'>
@@ -76,7 +77,7 @@ const NotiList = ({ setHasNewNotification }: NotiListProps) => {
           notifications.map((noti) => (
             <li
               key={noti.id}
-              className='hover:bg-primary-100 rounded-[6px] transition-colors duration-150'
+              className='hover:bg-primary-100 rounded-[0.375rem] transition-colors duration-150'
             >
               <NotiItem
                 id={noti.id}
