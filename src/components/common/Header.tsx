d3f7f5c5711d84da3943bad6019888f63d7f2888
geesunny <jiseonyang08@gmail.com>
2025-08-04 12:09:05 +0900

    ♻️refactor: logout route to home

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
   const handleLogout = () => {
     clearAuthStore();
     useAuthStore.persist.clearStorage();
-    router.push('/');
+    router.push('/home');
   };
 
   return (
